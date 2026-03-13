77d9d0ee50a3fe2538166f8aa2254f5e2779cbc8
Uyên <uyenchu98@gmail.com>
2026-03-13 22:27:48 +0700

    initial back-end

---
+++ src/main/java/com/backend/pishop/PishopApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PishopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/backend/pishop/entity/Account.java
@@ -0,0 +1,14 @@
+package com.backend.pishop.entity;
+
+
+import java.time.LocalDateTime;
+
+public class Account {
+	String email;
+	String passWord;
+	boolean isActive;
+	LocalDateTime deleteAt;
+	LocalDateTime creatAt;
+	
+
+}
